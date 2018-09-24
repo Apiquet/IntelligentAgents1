ccacacbc823440cc2f80a97370e75a22b5af67ea
Apiquet <anthony.piquet2@gmail.com>
2018-09-24 15:45:05 +0200

    Adding world size

--- rabbits/rabbits/piquet-erbacher-ex1/src/RabbitsGrassSimulationModel.java
+++ rabbits/rabbits/piquet-erbacher-ex1/src/RabbitsGrassSimulationModel.java
@@ -14,55 +14,75 @@ import uchicago.src.sim.engine.SimModelImpl;
 public class RabbitsGrassSimulationModel extends SimModelImpl {		
 
 	private Schedule schedule;
+	
+	//Number of agent
 	private int numAgents;
-	public static void main(String[] args) {
-			
-			System.out.println("Rabbit skeleton");
-			
-		}
+	
+	//World size	
+	private int worldXSize;
+	private int worldYSize;
+	
+	public static void main(String[] args) {			
+			System.out.println("Rabbit skeleton");			
+	}
 
-		public void begin() {
-			// TODO Auto-generated method stub
-			buildModel();
-		    buildSchedule();
-		    buildDisplay();
-		}
+	public void begin() {
+		// TODO Auto-generated method stub
+		buildModel();
+		buildSchedule();
+		buildDisplay();
+	}
 
-		public String[] getInitParam() {
-			// TODO Auto-generated method stub
-			String[] initParams = { "NumAgents", "AgentStrength" };
-		    return initParams;
-		}
-		public int getNumAgents(){
-		    return numAgents;
-		}
+	public String[] getInitParam() {
+		// TODO Auto-generated method stub
+		String[] initParams = { "NumAgents", "WorldXSize", "WorldYSize" };
+		return initParams;
+	}
+	public int getNumAgents(){
+		return numAgents;
+	}
 
-		public void setNumAgents(int na){
-		    numAgents = na;
-		}
-		public String getName() {
-			// TODO Auto-generated method stub
-			return "Rabbit Grass Simulation";
-		}
+	public void setNumAgents(int na){
+		numAgents = na;
+	}
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "Rabbit Grass Simulation";
+	}
 
-		public Schedule getSchedule() {
-			// TODO Auto-generated method stub
-			return schedule;
-		}
+	public Schedule getSchedule() {
+		// TODO Auto-generated method stub
+		return schedule;
+	}
 
-		public void setup() {
-			// TODO Auto-generated method stub
-			
-		}
-		public void buildModel(){
-		
-		}
+	public void setup() {
+		// TODO Auto-generated method stub
 
-		public void buildSchedule(){
-		  
-		}
+	}
+	public void buildModel(){
 
-		public void buildDisplay(){
-		  
-		}
+	}
+
+	public void buildSchedule(){
+
+	}
+	public int getWorldXSize(){
+		return worldXSize;
+	}
+
+	public void setWorldXSize(int wxs){
+		worldXSize = wxs;
+	}
+
+	public int getWorldYSize(){
+		return worldYSize;
+	}
+
+	public void setWorldYSize(int wys){
+		worldYSize = wys;
+	}
+
+	public void buildDisplay(){
+
+	}
 }
